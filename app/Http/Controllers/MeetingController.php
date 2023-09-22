73bf825be8b4ddf23aa5b196869b6bd4ae84abe6
anojdinuranga <anojdinuranga2001@gmail.com>
2023-09-22 11:01:09 +0530

    New Server Build

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -8,6 +8,7 @@ use GuzzleHttp\Exception\BadResponseException;
 use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use GuzzleHttp\Psr7;
 
 class MeetingController extends Controller
 {
@@ -48,8 +49,38 @@ class MeetingController extends Controller
         ]);
 
         try {
-            $response = $client->request('POST', $all,['json' => $request->all()]);
-            return response()->json(json_decode($response->getBody()->getContents()));
+            $contentType = $request->header('Content-Type');
+            
+            if (strpos($contentType, 'multipart/form-data') !== false) {
+                
+                $multipartData = [];
+
+                foreach ($request->all() as $name => $contents) {
+                    if ($contents instanceof \Illuminate\Http\UploadedFile && $contents->isValid()) {
+                        // If it's an instance of UploadedFile and the file is valid, it's an uploaded file
+                        $multipartData[] = [
+                            'name'     => $name,
+                            'contents' => fopen($contents->getRealPath(), 'r'), // Open the file for reading
+                            'filename' => $contents->getClientOriginalName(),  // Set the original filename
+                        ];
+                    }else{
+                        $multipartData[] = [
+                            'name'     => $name,
+                            'contents' => $contents,
+                        ];
+                    }
+                }
+
+                $response = $client->request('POST', $all, [
+                    'multipart' => $multipartData,
+                ]);
+
+                return response()->json(json_decode($response->getBody()->getContents()));
+            } else {
+                $response = $client->request('POST', $all,['json' => $request->all()]);
+                return response()->json(json_decode($response->getBody()->getContents()));
+            }
+            
         } catch (BadResponseException  $e) {
             $response = $e->getResponse();
             return response()->json([
